9f3b650001ece6aceaa95ada4214c236501c4ee4
Marcin Grawiński <116593541+Grawikos@users.noreply.github.com>
2023-10-19 10:28:11 +0200

    Update TestExtStack.cpp
    final

--- extendedStackCPP/TestExtStack.cpp
+++ extendedStackCPP/TestExtStack.cpp
@@ -1,26 +1,40 @@
 #include "ExtStack.h"
 #include <iostream>
 
+void empty(Stack &s){
+	while(!s.isEmpty()){
+		std::cout << s.pop() << " ";
+	}
+	std::cout << std::endl;
+}
+
 int main()
 {
-	Stack firstS;
-
+	Stack firstS, secondS, thirdS, fourthS;
 	for (int i = 1; i <= 10; i++) {
 		firstS.push(i);
 	}
+	for (int i = 1; i <= 1000; i++) {
+		secondS.push(i);
+	}
 	
-	Stack secondS(firstS);
-
+	firstS = firstS;
+	secondS = firstS;
+	thirdS = firstS;
+	Stack fifthS(secondS);
+	fourthS = fifthS;
+	
 	std::cout << "Popped elements (first): ";
-	while (!firstS.isEmpty()) {
-		std::cout << firstS.pop() << " ";
-	}
+	empty(firstS);
 	std::cout << "\nPopped elements (second): ";
-	while (!secondS.isEmpty()) {
-		std::cout << secondS.pop() << " ";
-	}
-
-	std::cout << std::endl;
+	empty(secondS);
+	std::cout << "\nPopped elements (3): ";
+	empty(thirdS);
+	std::cout << "\nPopped elements (4): ";
+	empty(fourthS);
+	std::cout << "\nPopped elements (5): ";
+	empty(fifthS);
+	
 
 	return 0;
 }
